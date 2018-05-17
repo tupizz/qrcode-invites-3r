0290f8507eb3081afa431883085eb630d120c7d1
Tadeu Tupinamba <tadeu.tupiz@gmail.com>
2018-05-17 02:24:28 -0300

    git ignore update

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBIPMKykDKYCjXOhZy_o9Cuv0MHg6ill2k",
+    authDomain: "qrcode-invites-3r.firebaseapp.com",
+    databaseURL: "https://qrcode-invites-3r.firebaseio.com",
+    projectId: "qrcode-invites-3r",
+    storageBucket: "qrcode-invites-3r.appspot.com",
+    messagingSenderId: "468416487988"
+};
